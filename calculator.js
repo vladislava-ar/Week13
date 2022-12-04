bdf30e6a34d41731350a03335a9b8228819aaa9e
vladislava-ar <vladislava.dnk@gmail.com>
2022-12-04 21:23:41 +0300

    add project

---
+++ calculator.js
@@ -0,0 +1,23 @@
+    function add() {
+        let a = prompt("Введите первое число", 'Число1');
+        let b = prompt("Введите второе число", 'Число2');
+        alert(+a + +b);
+    }
+    
+    function subtract() {
+        let a = prompt("Введите первое число", 'Число1');
+        let b = prompt("Введите второе число", 'Число2');
+        alert(a - b);
+    }
+
+    function multiply() {
+        let a = prompt("Введите первое число", 'Число1');
+        let b = prompt("Введите второе число", 'Число2');
+        alert(a * b);
+    }
+
+    function divide() {
+        let a = prompt("Введите первое число", 'Число1');
+        let b = prompt("Введите второе число", 'Число2');
+        alert(a / b);
+    }
